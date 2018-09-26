b0f274991141b83ff0b352a02de790b05b149c7e
baco16g <baco.16g@gmail.com>
2018-09-27 02:08:46 +0900

    feat(one-time-password): set velifyOneTimePassword

--- one-time-password/client/src/api/index.ts
+++ one-time-password/client/src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import firebase from 'firebase'
 import * as config from '../../config.json'
 
 export const createUser = ({ phone }: { phone: string }) => {
@@ -15,4 +16,25 @@ export const requestOneTimePassword = ({ phone }: { phone: string }) => {
       phone
     })
     .catch(error => window.console.log(error))
+}
+
+export const velifyOneTimePassword = ({
+  phone,
+  code
+}: {
+  phone: string
+  code: string
+}) => {
+  axios
+    .post(`${config.api_root}/velifyOneTimePassword`, {
+      phone,
+      code
+    })
+    .then(res => {
+      return res.data
+    })
+    .then(data => {
+      firebase.auth().signInWithCustomToken(data.token)
+    })
+    .catch(err => window.console.log(err))
 }
