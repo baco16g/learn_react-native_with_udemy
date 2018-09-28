40964cd86de605c94167682afb7a4b3746110cf4
baco16g <baco.16g@gmail.com>
2018-09-28 23:20:54 +0900

    feat(jobs): setting stack navigation config for Review screen.

--- jobs/src/screens/ReviewScreen.tsx
+++ jobs/src/screens/ReviewScreen.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
+import { compose, ComponentEnhancer, setStatic } from 'recompose'
+import { Button } from 'react-native-elements'
 
 const ReviewScreen = () => (
   <View>
@@ -11,4 +13,21 @@ const ReviewScreen = () => (
   </View>
 )
 
-export default ReviewScreen
+const enhancer: ComponentEnhancer<any, any> = compose(
+  setStatic('navigationOptions', ({ navigation }: { navigation: any }) => ({
+    title: 'Review Jobs',
+    headerRight: (
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate('settings')}
+        backgroundColor="rgba(0, 0, 0, 0)"
+        color="rgba(0, 122, 255, 1)"
+      />
+    ),
+    headerStyle: {
+      marginTop: Platform.OS === 'android' ? 24 : 0
+    }
+  }))
+)
+
+export default enhancer(ReviewScreen)
