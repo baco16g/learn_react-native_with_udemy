0ba63cdcb645f3347a6775bf16371dc61d14d720
baco16g <baco.16g@gmail.com>
2018-09-29 21:22:17 +0900

    feat(jobs): trigger onAuthComplete when screnn mounted & updated

--- jobs/src/screens/AuthScreen.tsx
+++ jobs/src/screens/AuthScreen.tsx
@@ -1,45 +1,57 @@
 import * as React from 'react'
-import { View, Text, AsyncStorage } from 'react-native'
+import { View, Text } from 'react-native'
+import { NavigationScreenProp } from 'react-navigation'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
-import { compose, ComponentEnhancer, lifecycle } from 'recompose'
+import { compose, ComponentEnhancer, lifecycle, withHandlers } from 'recompose'
 import { $Call } from 'utility-types'
 
 import * as actions from '../actions'
+import { IRootStore } from '../reducers'
+
+interface IOuterProps {
+  navigation: NavigationScreenProp<any>
+}
 
 interface IPropsConnected
   extends $Call<typeof mapDispatchToProps>,
     $Call<typeof mapStateToProps> {}
 
-const AuthScreen = () => (
-  <View>
-    <Text>AuthScreen</Text>
-    <Text>AuthScreen</Text>
-    <Text>AuthScreen</Text>
-    <Text>AuthScreen</Text>
-    <Text>AuthScreen</Text>
-  </View>
-)
-
-const mapStateToProps = () => ({})
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    actions: {
-      ...bindActionCreators(actions, dispatch)
-    }
-  }
+interface IHandlers {
+  onAuthComplete: (token: string | null) => void
 }
 
-const enhancer: ComponentEnhancer<{}, {}> = compose(
+const AuthScreen = () => <View />
+
+const mapStateToProps = ({ auth }: IRootStore) => ({
+  token: auth.token
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  actions: {
+    ...bindActionCreators(actions, dispatch)
+  }
+})
+
+const enhancer: ComponentEnhancer<{}, IOuterProps> = compose(
   connect(
     mapStateToProps,
     mapDispatchToProps
   ),
-  lifecycle<IPropsConnected, {}>({
+  withHandlers<IOuterProps & IPropsConnected, IHandlers>({
+    onAuthComplete: ({ navigation }) => (token: string | null) => {
+      if (token) {
+        navigation.navigate('map')
+      }
+    }
+  }),
+  lifecycle<IOuterProps & IPropsConnected & IHandlers, {}>({
     componentDidMount() {
       this.props.actions.facebookLogin()
-      AsyncStorage.removeItem('fb_token')
+      this.props.onAuthComplete(this.props.token)
+    },
+    componentWillReceiveProps(nextProps) {
+      this.props.onAuthComplete(nextProps.token)
     }
   })
 )
