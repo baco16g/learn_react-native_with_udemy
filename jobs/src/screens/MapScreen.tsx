2d513bf0aaf1a0871211e853e6fef3076940f7ed
baco16g <baco.16g@gmail.com>
2018-09-30 01:12:50 +0900

    feat(jobs): attach MapView to MapScreen

--- jobs/src/screens/MapScreen.tsx
+++ jobs/src/screens/MapScreen.tsx
@@ -1,14 +1,70 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
+import {
+  compose,
+  ComponentEnhancer,
+  withState,
+  lifecycle,
+  withHandlers,
+  withStateHandlers
+} from 'recompose'
+import { MapView } from 'expo'
 
-const MapScreen = () => (
-  <View>
-    <Text>MapScreen</Text>
-    <Text>MapScreen</Text>
-    <Text>MapScreen</Text>
-    <Text>MapScreen</Text>
-    <Text>MapScreen</Text>
-  </View>
+interface IProps extends IState, IStateUpdater {}
+
+interface IRegion {
+  longitude: number
+  latitude: number
+  longitudeDelta: number
+  latitudeDelta: number
+}
+
+interface IState {
+  region: IRegion
+  mapLoaded: boolean
+}
+
+interface IStateUpdater {
+  onRegionChangeComplete: (region: IRegion) => void
+}
+
+const MapScreen = ({ region, mapLoaded, onRegionChangeComplete }: IProps) =>
+  mapLoaded ? (
+    <View style={{ flex: 1 }}>
+      <MapView
+        region={region}
+        onRegionChangeComplete={onRegionChangeComplete}
+        style={{ flex: 1 }}
+      />
+    </View>
+  ) : (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <ActivityIndicator size="large" />>
+    </View>
+  )
+
+const enhancer: ComponentEnhancer<IProps, {}> = compose(
+  withState('mapLoaded', '', false),
+  withStateHandlers(
+    {
+      region: {
+        longitude: -122,
+        latitude: 37,
+        longitudeDelta: 0.04,
+        latitudeDelta: 0.09
+      }
+    },
+    {
+      onRegionChangeComplete: () => (region: IRegion) => ({
+        region
+      })
+    }
+  ),
+  lifecycle({
+    componentDidMount() {
+      this.setState({ mapLoaded: true })
+    }
+  })
 )
 
-export default MapScreen
+export default enhancer(MapScreen)
