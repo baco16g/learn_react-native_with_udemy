db10d1732967da121c974b8a89543b07de1a2385
baco16g <baco.16g@gmail.com>
2018-09-30 22:25:59 +0900

    feat(jobs): create Setting Screen

--- jobs/src/screens/SettingScreen.tsx
+++ jobs/src/screens/SettingScreen.tsx
@@ -1,14 +1,53 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { Dispatch, bindActionCreators } from 'redux'
+import { compose, ComponentEnhancer, withHandlers } from 'recompose'
+import { $Call } from 'utility-types'
+import { Button } from 'react-native-elements'
+import { IRootStore } from '../reducers'
+import * as actions from '../actions'
 
-const SettingScreen = () => (
+type Props = IPropsConnected & IHandlers
+
+interface IPropsConnected
+  extends $Call<typeof mapDispatchToProps>,
+    $Call<typeof mapStateToProps> {}
+
+interface IHandlers {
+  clearLikedJobs: () => void
+}
+
+const SettingScreen = ({ clearLikedJobs }: Props) => (
   <View>
-    <Text>SettingScreen</Text>
-    <Text>SettingScreen</Text>
-    <Text>SettingScreen</Text>
-    <Text>SettingScreen</Text>
-    <Text>SettingScreen</Text>
+    <Button
+      title="Reset Liked Jobs"
+      large
+      icon={{ name: 'delete-forever' }}
+      backgroundColor="#F44336"
+      onPress={clearLikedJobs}
+    />
   </View>
 )
 
-export default SettingScreen
+const mapStateToProps = (state: IRootStore) => ({})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  actions: {
+    ...bindActionCreators(actions, dispatch)
+  }
+})
+
+const enhancer: ComponentEnhancer<Props, {}> = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withHandlers<IPropsConnected, IHandlers>({
+    clearLikedJobs: ({ actions: { clearLikedJobs } }) => () => {
+      clearLikedJobs()
+    }
+  })
+)
+
+export default enhancer(SettingScreen)
