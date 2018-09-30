4371d3bc1de8c4cb8353eacbbd898e2d83b469f9
baco16g <baco.16g@gmail.com>
2018-09-30 17:01:41 +0900

    feat(jobs): create reducer for job

--- jobs/src/reducers/index.ts
+++ jobs/src/reducers/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import * as auth from './auth'
+import * as job from './job'
 
 export interface IRootStore {
   auth: auth.IState
+  job: job.IState
 }
 
 export default combineReducers<IRootStore>({
-  auth: auth.reducer
+  auth: auth.reducer,
+  job: job.reducer
 })
---
+++ jobs/src/reducers/job.ts
@@ -0,0 +1,19 @@
+import { reducerWithInitialState } from 'typescript-fsa-reducers'
+import * as asyncActions from '../actions/job/async'
+
+export interface IState {
+  list: [] | Job[]
+}
+
+const initialState: IState = {
+  list: []
+}
+
+export const reducer = reducerWithInitialState(initialState)
+  .case(asyncActions.fetchJobs.done, (state, { result }) => ({
+    ...state,
+    list: result.list
+  }))
+  .case(asyncActions.fetchJobs.failed, state => ({
+    ...state
+  }))
