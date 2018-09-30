db0b152b7294ee423e3d50dd8e638ecdcb7afe18
baco16g <baco.16g@gmail.com>
2018-09-30 16:40:35 +0900

    feat(jobs): abstract Job type that is response from github's job API

---
+++ jobs/typings/api/index.d.ts
@@ -0,0 +1,11 @@
+declare interface Job {
+  id: string
+  location: string
+  company: string
+  company_logo: string
+  title: string
+  description: string
+  how_to_apply: string
+  type: string
+  url: string
+}
