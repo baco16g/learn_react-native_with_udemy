331beb47b828722b194aed2fc0fe40d9e35e90b0
baco16g <baco.16g@gmail.com>
2018-09-30 22:25:12 +0900

    feat(jobs): create ReviewScreen

--- jobs/src/screens/ReviewScreen.tsx
+++ jobs/src/screens/ReviewScreen.tsx
@@ -1,17 +1,59 @@
 import * as React from 'react'
-import { View, Text, Platform } from 'react-native'
+import {
+  View,
+  ScrollView,
+  Text,
+  Platform,
+  StyleSheet,
+  Linking
+} from 'react-native'
 import { compose, ComponentEnhancer, setStatic } from 'recompose'
-import { Button } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import { Card, Button } from 'react-native-elements'
+import FitImage from 'react-native-fit-image'
+import { $Call } from 'utility-types'
+import { IRootStore } from '../reducers'
 
-const ReviewScreen = () => (
-  <View>
-    <Text>ReviewScreen</Text>
-    <Text>ReviewScreen</Text>
-    <Text>ReviewScreen</Text>
-    <Text>ReviewScreen</Text>
-    <Text>ReviewScreen</Text>
-  </View>
-)
+type Props = IPropsConnected
+
+interface IPropsConnected
+  extends $Call<typeof mapDispatchToProps>,
+    $Call<typeof mapStateToProps> {}
+
+const ReviewScreen = ({ likedJobs }: Props) => {
+  const renderLikedJobs = () =>
+    likedJobs.map(job => (
+      <Card title={job.title} key={job.id}>
+        <View>
+          {job.company_logo && (
+            <FitImage
+              source={{ uri: job.company_logo }}
+              resizeMode="contain"
+              style={styles.logo}
+            />
+          )}
+          <View style={styles.detailWrapper}>
+            <Text style={styles.italics}>{job.company}</Text>
+            <Text style={styles.italics}>{job.location}</Text>
+          </View>
+          <Button
+            title="Apply Now!"
+            backgroundColor="#03A9F4"
+            onPress={() => Linking.openURL(job.url)}
+          />
+        </View>
+      </Card>
+    ))
+
+  return <ScrollView>{renderLikedJobs()}</ScrollView>
+}
+
+const mapStateToProps = (state: IRootStore) => ({
+  likedJobs: state.like.list
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
 const enhancer: ComponentEnhancer<any, any> = compose(
   setStatic('navigationOptions', ({ navigation }: { navigation: any }) => ({
@@ -27,7 +69,31 @@ const enhancer: ComponentEnhancer<any, any> = compose(
     headerStyle: {
       marginTop: Platform.OS === 'android' ? 24 : 0
     }
-  }))
+  })),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
 )
+
+const styles = StyleSheet.create({
+  detailWrapper: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  logo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 100,
+    marginBottom: 30
+  },
+  italics: {
+    fontStyle: 'italic'
+  }
+})
 
 export default enhancer(ReviewScreen)
